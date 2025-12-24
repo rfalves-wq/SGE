2a364c1a2b9ad7b9c3c939fe8a87a23936390aa0
User <rogerioparacambi@gmail.com>
2025-12-24 19:24:37 -0300

    meu primeiro commit

---
+++ brands/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Brand(models.Model):
+    name = models.CharField(max_length=500)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']  # Ordena pelo nome do paciente
+
+    # Como o paciente será exibido no admin e no shell
+    def __str__(self):
+        return self.name
